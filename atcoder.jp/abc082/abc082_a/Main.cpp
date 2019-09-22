39778afb386e9824817de8db4e44f3870a610366
Yellow13 <takuson2525zt@gmail.com>
2019-09-22 11:46:38 +0900

    [AC] abc082 abc082_a

---
+++ atcoder.jp/abc082/abc082_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a;
+    cin >> b;
+    string f;
+    int c = (a + b)/ 2 + (a + b) % 2;
+    cout << c;
+}
